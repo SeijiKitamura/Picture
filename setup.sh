f50d8346fca481e7824992f3d91e3ccae5d13370
seiji.kitamura <seiji.kitamura@gmail.com>
2016-12-31 18:40:35 +0900

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd
 sudo apt-get install -y fswebcam apache2
 
 #apache2
-sudo cp raspberry/picture.conf /etc/apache2/sites-available
+sudo cp ./conf/picture.conf /etc/apache2/sites-available
 sudo a2ensite picture
 sudo /etc/init.d/apache2 restart
 
@@ -15,6 +15,6 @@ git clone git@172.16.0.13:/home/git/picture.git
 
 #cron
 cd picture
-crontab cron.txt
-su -c "crontab reboot.txt"
+crontab ./conf/cron.txt
+su -c "crontab ./conf/reboot.txt"
 
