f7cd9b50331859f9a4fa55ecd51c4dee8d14f5a6
seiji.kitamura <seiji.kitamura@gmail.com>
2017-01-01 16:29:21 +0900

    changed send.sh

--- send.sh
+++ send.sh
@@ -1,46 +1,67 @@
 #! /bin/sh
 
-#変数セット
-BACKDAY=3                 #何日前からのデータをサーバーに送信するかセット
-SAVEDIR=`pwd`/img/DAY     #ローカル写真保存場所
+#何日前からのデータをサーバーに送信するかセット
+BACKDAY=1
 
-HOSTNAME="raspberrypi1"
-FILESERVER=172.16.0.12    #ファイルサーバーアドレス
-SCPUSER=kennpin1          #ファイルサーバーへのログインID
-DIR=/mnt/usb2/samba/img   #ファイルサーバー上のPath
+#PC名をセット
+HOSTNAME=`hostname`
 
-HOSTNAME2="raspberry1"
-FILESERVER2="172.16.0.161"
-SCPUSER2="kennpin1"          #ファイルサーバーへのログインID
-DIR2="/home/kennpin1/ドキュメント/RubyOnRails/spade/public/pictures"
+cd `dirname $0`
 
-#画像ディレクトリへ移動
-SHPATH=`which $0`
-REALDIR=`dirname ${SHPATH}`
-cd ${REALDIR}/img
+#ローカルの写真をセーブする場所をセット
+SAVEDIR=`pwd`/img
+
+#iniファイルのディレクトリをセット
+INIDIR=`pwd`/ini
+
+#保存場所を確認
+if [ ! -d "$SAVEDIR" ]; then
+  echo "error ${SAVEDIR}がありません..."
+  exit 1
+fi
+
+if [ ! -d "$INIDIR" ]; then
+  echo "error iniディレクトリがありません。処理を中止します"
+  exit 1
+fi
+
+#SCP用iniファイル存在チェック
+if ls ${INIDIR}/scpserver*.ini > /dev/null 2>&1
+then
+  echo "SCP送信先設定ファイル(${INIDIR}/scpserver*.iniの存在を確認しました"
+else
+  echo "error SCP送信先設定ファイルが存在しません。"
+  exit 1
+fi
 
 #処理開始
-for i in $(seq 0 ${BACKDAY});do
-  HIDUKE=`date +"%Y%m%d" --date "${i} days ago"`
-  TOSI=`date -d ${HIDUKE} +"%Y"`
-  TUKI=`date -d ${HIDUKE} +"%m"`
-  HI=`date -d ${HIDUKE} +"%d"`
-  for VDO in `ls /dev| grep video[0-3]`;do
-   #送信ファイル確定
-   SENDFILE="DAY${HIDUKE}*_${VDO}.jpg"
+for SCPINI in `ls ${INIDIR}/scpserver*.ini`
+do
+  #設定ファイル読み込み
+  . ${SCPINI}
 
-   #ファイルサーバー保存場所確定
-   SCPDIR=${SCPUSER}@${FILESERVER}:${DIR}/${TOSI}/${TUKI}/${HI}/${HOSTNAME}/${VDO}
-   SCPDIR2=${SCPUSER2}@${FILESERVER2}:${DIR2}/${TOSI}/${TUKI}/${HI}/${HOSTNAME2}/${VDO}
+  for i in $(seq 0 ${BACKDAY});do
+    HIDUKE=`date +"%Y%m%d" --date "${i} days ago"`
+    TOSI=`date -d ${HIDUKE} +"%Y"`
+    TUKI=`date -d ${HIDUKE} +"%m"`
+    HI=`date -d ${HIDUKE} +"%d"`
 
-   #ファイル送信
-   find . -maxdepth 1 -type f -name "${SENDFILE}" | xargs -I{} scp -p {} ${SCPDIR} > /dev/null
+    for vdo in `ls /dev| grep -E 'video[0-9]+$'`;do
+      #送信ファイル確定
+      SENDFILE="DAY${HIDUKE}*_${vdo}.jpg"
 
-   #ファイル送信
-   find . -maxdepth 1 -type f -name "${SENDFILE}" | xargs -I{} scp -p {} ${SCPDIR2} > /dev/null
+      #ファイルサーバー保存場所確定
+      SCPDIR=${SCPUSER}@${FILESERVER}:${DIR}/${TOSI}/${TUKI}/${HI}/${HOSTNAME}/${vdo}
+
+      #ファイル送信
+      echo "ファイル送信開始 ${SCPDIR}"
+      find ${SAVEDIR} -maxdepth 1 -type f -name "${SENDFILE}" | xargs -I{} scp -p {} ${SCPDIR} 2>&1
+    done
   done
 done
 
 #古い画像を削除
 HIDUKE=`date +"%Y%m%d" --date "${BACKDAY} days ago"`
-find . -maxdepth 1 -type f -name "DAY${HIDUKE}*.jpg" -delete
+echo "日付 ${HIDUKE}の写真を削除します"
+find ${SAVEDIR} -maxdepth 1 -type f -name "DAY${HIDUKE}*.jpg" -delete
+echo "日付 ${HIDUKE}の写真を削除しました"
