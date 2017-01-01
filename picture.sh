9e8d4a396df2f80408a15342974b4ec916f483b3
seiji.kitamura <seiji.kitamura@gmail.com>
2017-01-01 16:28:42 +0900

    changed picture.sh

--- picture.sh
+++ picture.sh
@@ -1,19 +1,43 @@
 #! /bin/sh
 
-#変数セット
-SAVEDIR=`pwd`/img/DAY     #ローカル写真保存場所
+#撮影間隔をセット(秒)
+#(0にするとたまにカメラが掴めなくてエラーになる)
+SLEEPTIME=3
 
-HOSTNAME="raspberrypi1"
-FILESERVER=172.16.0.12    #ファイルサーバーアドレス
-SCPUSER=kennpin1          #ファイルサーバーへのログインID
-DIR=/mnt/usb2/samba/img   #ファイルサーバー上のPath
-
-HOSTNAME2="raspberry1"
-FILESERVER2="172.16.0.161"
-SCPUSER2="kennpin1"          #ファイルサーバーへのログインID
-DIR2="/home/kennpin1/ドキュメント/RubyOnRails/spade/public/pictures"
+#PC名をセット
+HOSTNAME=`hostname`
 
 cd `dirname $0`
+
+#iniファイルのディレクトリをセット
+INIDIR=`pwd`/ini
+
+#ローカル写真保存場所
+SAVEDIR=`pwd`/img
+
+#保存場所を作成
+if [ ! -d "$SAVEDIR" ]; then
+  mkdir $SAVEDIR
+fi
+
+if [ ! -d "$INIDIR" ]; then
+  echo "error iniディレクトリがありません。処理を中止します"
+  exit 1
+fi
+
+if [ ! -e "$INIDIR"/video*.ini ]; then
+  echo "error カメラ設定ファイルがありません。処理を中止します。"
+  exit 1
+fi
+
+#SCP用iniファイル存在チェック
+if ls ${INIDIR}/scpserver*.ini > /dev/null 2>&1
+then
+  echo "SCP送信先設定ファイル(${INIDIR}/scpserver*.iniの存在を確認しました"
+else
+  echo "error SCP送信先設定ファイルが存在しません。"
+  exit 1
+fi
 
 #ファイル名に使用する日付をゲット
 TOSI=`date +%Y`
@@ -23,37 +47,8 @@ JI=`date '+%H'`
 FUN=`date '+%M'`
 HIDUKE=${TOSI}${TUKI}${HI}${JI}${FUN}
 
-#ローカルの写真をセーブする場所をセット
-SAVEDIR=`pwd`/img
-
-#iniファイルのディレクトリをセット
-INIDIR=`pwd`/ini
-
-#logファイルのディレクトリをセット
-LOGDIR=`pwd`/log
-
-if [ ! -d "$INIDIR" ]; then
-  echo "iniディレクトリがありません。処理を中止します"
-  exit 1
-fi
-
-if [ ! -e "$INIDIR"/video*.ini ]; then
-  echo "カメラ設定ファイルがありません。処理を中止します。"
-  exit 1
-fi
-
-#保存場所を作成
-if [ ! -d "$SAVEDIR" ]; then
-  mkdir $SAVEDIR
-fi
-
-#ログの保存場所を作成
-if [ ! -d "$LOGDIR" ]; then
-  mkdir $LOGDIR
-fi
-
 #撮影開始
-for vdo in `ls /dev | grep video[0-3]`
+for vdo in `ls /dev | grep -E 'video[0-9]+$'`
 do
   #iniファイルの存在チェック
   #なければ./ini/video_default.iniを使用
@@ -63,24 +58,41 @@ do
    INIFILE=${INIDIR}/video_default.ini
   fi
 
-  echo "------------------------------------------" >> log/fswebcam.log
-  echo `date +"%Y-%m-%d %H:%M:%S "` "START"         >> log/fswebcam.log
-  /usr/bin/fswebcam -d /dev/${vdo} -c ${INIFILE} --title ${HOSTNAME}_${vdo} --save ${SAVEDIR}/DAY${HIDUKE}_${vdo}.jpg
+  echo "${vdo}の撮影開始"
+
+  /usr/bin/fswebcam -q -d /dev/${vdo} -c ${INIFILE} --title ${HOSTNAME}_${vdo} --save ${SAVEDIR}/DAY${HIDUKE}_${vdo}.jpg
+
+  if [ $? -ne 0 ]; then
+    echo "error ${vdo}で撮影できませんでした...."
+  fi
+
+  echo "sleeping ${SLEEPTIME}sec"
+  sleep ${SLEEPTIME}
 done
 
 #ファイル送信
-for vdo in `ls /dev | grep video[0-3]`
+for SCPINI in `ls ${INIDIR}/scpserver*.ini`
 do
-  #送信ファイル
-  SENDFILE=${SAVEDIR}/DAY${HIDUKE}_${vdo}.jpg
+  #設定ファイル読み込み
+  . ${SCPINI}
 
-  #ファイルサーバー保存場所確定
-  SCPDIR=${SCPUSER}@${FILESERVER}:${DIR}/${TOSI}/${TUKI}/${HI}/${HOSTNAME}/${vdo}
-  SCPDIR2=${SCPUSER2}@${FILESERVER2}:${DIR2}/${TOSI}/${TUKI}/${HI}/${HOSTNAME2}/${vdo}
+  for vdo in `ls /dev | grep -E 'video[0-9]+$'`
+  do
+    #送信ファイル
+    SENDFILE=${SAVEDIR}/DAY${HIDUKE}_${vdo}.jpg
 
-  #ファイル送信
-  if [ -e ${SENDFILE} ]; then
-    scp -p ${SENDFILE} ${SCPDIR} >/dev/null
-    scp -p ${SENDFILE} ${SCPDIR2} >/dev/null
-  fi
+    if [ -e ${SENDFILE} ]; then
+      #ファイルサーバー保存場所確定
+      SCPDIR=${SCPUSER}@${FILESERVER}:${DIR}/${TOSI}/${TUKI}/${HI}/${HOSTNAME}/${vdo}
+
+      #ファイル送信
+      scp -p ${SENDFILE} ${SCPDIR} 2>&1
+
+      if [ $? -eq 0 ]; then
+        echo "success scp ${SENDFILE} ${SCPDIR}"
+      else
+        echo "error scp ${SENDFILE} ${SCPDIR}"
+      fi
+    fi
+  done
 done
